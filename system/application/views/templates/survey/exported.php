50c973e37240d8537909a93914788d180de25448
Pelolito Aparece <paparece@lrdc.lexmark.ds>
2018-06-03 22:45:50 +0800

    apars 06/03/2018 - introtext class in exported view.

--- system/application/views/templates/survey/exported.php
+++ system/application/views/templates/survey/exported.php
@@ -1,5 +1,5 @@
 <div class="col-md-12">    
-  <h2 style="text-align: center"><br>
+  <h2 class="introtext" style="text-align: center"><br>
     <?php echo $export_result ?><br>
   </h2>
   <?php header("Refresh: 2; url=".base_url()); ?>
